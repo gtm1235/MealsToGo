703a2e015e08ef6178ba4b1294df40261c964356
Gennaro Maida <gtm1235@gmail.com>
2023-10-12 21:51:14 -0500

    Set Up Linting

--- App.js
+++ App.js
@@ -1,25 +1,44 @@
-import { StatusBar as ExpoStatusBar} from 'expo-status-bar';
-import { StatusBar, Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import {
+  StatusBar,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
-const isAndroid = Platform.OS === 'android'
+const isAndroid = Platform.OS === "android";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={{ flex: 1, marginTop: isAndroid ?  StatusBar.currentHeight  : 0,  }}>
-        <View style={{ padding: 16, backgroundColor: 'green',}}>
-          <Text >Our react native blank canvas</Text>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Text>Search</Text>
         </View>
-        <View style={{ padding: 16, backgroundColor: 'blue', flex: 1,  }}>
-          <Text >Our react native blank canvas</Text>
+        <View style={styles.list}>
+          <Text>Body</Text>
         </View>
       </SafeAreaView>
-      <ExpoStatusBar style='auto'/>
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
 
 const styles = StyleSheet.create({
-
-},
-);
+  container: {
+    flex: 1,
+    marginTop: isAndroid ? StatusBar.currentHeight : 0,
+  },
+  search: {
+    padding: 16,
+    backgroundColor: "green",
+  },
+  list: {
+    padding: 16,
+    backgroundColor: "blue",
+    flex: 1,
+  },
+});
