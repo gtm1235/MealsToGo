b8a85f20e1abbea4ad338ba7fd43e2e3b5549143
Gennaro Maida <gtm1235@gmail.com>
2023-10-13 11:26:47 -0500

    added styled-components

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,40 +1,34 @@
 import React from "react";
+import styled from "styled-components";
 import { Searchbar } from "react-native-paper";
-import {
-  StatusBar,
-  Platform,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import { StatusBar, Platform, SafeAreaView } from "react-native";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 
 const isAndroid = Platform.OS === "android";
 
-export const RestaurantsScreen = () => (
-  <SafeAreaView style={styles.container}>
-    <View style={styles.search}>
-      <Searchbar placeholder="Search" />
-    </View>
-    <View style={styles.list}>
-      <RestaurantInfoCard />
-    </View>
-  </SafeAreaView>
-);
+const ScreenContainer = styled(SafeAreaView)`
+  flex: 1;
+  margin-top: ${isAndroid ? StatusBar.currentHeight + "px" : "0"};
+`;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: isAndroid ? StatusBar.currentHeight : 0,
-  },
-  search: {
-    padding: 16,
-    backgroundColor: "lightblue",
-  },
-  list: {
-    padding: 16,
-    backgroundColor: "blue",
-    flex: 1,
-  },
-});
+const CardList = styled.View`
+  padding: 16px;
+  background-color: blue;
+  flex: 1;
+`;
+
+const ScreenSearchBar = styled.View`
+  padding: 16px;
+  background-color: lightblue;
+`;
+
+export const RestaurantsScreen = () => (
+  <ScreenContainer>
+    <ScreenSearchBar>
+      <Searchbar placeholder="Search" />
+    </ScreenSearchBar>
+    <CardList>
+      <RestaurantInfoCard />
+    </CardList>
+  </ScreenContainer>
+);
