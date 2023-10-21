55afff618d2a911efb57a9f529ef0464f5e83394
Gennaro Maida <gtm1235@gmail.com>
2023-10-20 21:38:58 -0500

    added additional navigation in map screen

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -13,7 +13,7 @@ const Map = styled(MapView)`
   width: 100%;
 `;
 
-export const MapScreen = () => {
+export const MapScreen = ({ navigation }) => {
   const { location } = useContext(LocationContext);
   const { restaurants } = useContext(RestaurantsContext);
   const [latDelta, setLatDelta] = useState(0);
@@ -47,7 +47,13 @@ export const MapScreen = () => {
                   longitude: restaurant.geometry.location.lng,
                 }}
               >
-                <Callout>
+                <Callout
+                  onPress={() =>
+                    navigation.navigate("restaurantDetail", {
+                      restaurant,
+                    })
+                  }
+                >
                   <MapCallout restaurant={restaurant} />
                 </Callout>
               </Marker>
